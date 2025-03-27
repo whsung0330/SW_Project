017e592a10f4a31a71dbe40494c2c17b0f37768e
Dahye Won <doall1102@gmail.com>
2025-03-28 00:51:49 +0900

    feat: Strategy Pattern 적용하여 ShapeFactory의 생성 로직을 Shape 클래스로 위임

--- src/component/ShapeFactory.ts
+++ src/component/ShapeFactory.ts
@@ -9,19 +9,32 @@ interface ShapeProps {
   color: string;
 }
 
+interface ShapeCreator {
+  create(props: ShapeProps): Shape;
+}
+
+class RectangleCreator implements ShapeCreator {
+  create(props: ShapeProps): Shape {
+    return new Rectangle(props.id, props.startX, props.startY, props.endX, props.endY, props.color);
+  }
+}
+
+class EllipseCreator implements ShapeCreator {
+  create(props: ShapeProps): Shape {
+    return new Ellipse(props.id, props.startX, props.startY, props.endX, props.endY, props.color);
+  }
+}
+
 export class ShapeFactory {
+  private static creators: Record<string, ShapeCreator> = {
+    rectangle: new RectangleCreator(),
+    ellipse: new EllipseCreator(),
+  };
+
+
   static createShape(type: string, props: ShapeProps): Shape {
-    const { id, startX, startY, endX, endY, color } = props;
-
-    switch (type) {
-      case "rectangle":
-        return new Rectangle(id, startX, startY, endX, endY, color);
-
-      case "circle":
-        return new Ellipse(id, startX, startY, endX, endY, color);
-      
-      default:
-        throw new Error(`Unknown type shape ${type}`);
-    }
+    const creator = this.creators[type];
+    if (!creator) throw new Error(`Unknown type shape ${type}`);
+    return creator.create(props);
   }
 }
